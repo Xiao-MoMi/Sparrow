85b201adab2e30e781054897cec64d58abaedc4f
XiaoMoMi <972454774@qq.com>
2024-04-24 18:43:37 +0800

    [Update] Allow custom parameters

--- bukkit/src/main/java/net/momirealms/sparrow/bukkit/command/CommandConfig.java
+++ bukkit/src/main/java/net/momirealms/sparrow/bukkit/command/CommandConfig.java
@@ -38,19 +38,16 @@ public class CommandConfig {
         ArrayList<Command.@NonNull Builder<CommandSender>> builders = new ArrayList<>();
         for (String usage : getUsages()) {
             if (usage.startsWith("/")) {
-                String command = usage.substring(1);
+                String command = usage.substring(1).trim();
                 String[] split = command.split(" ");
-                if (split.length > 2) {
-                    throw new IllegalArgumentException("Invalid usage: " + usage);
-                }
                 for (String sub : split) {
                     if (sub.isEmpty() || sub.isBlank()) {
                         throw new IllegalArgumentException("Invalid usage: " + usage);
                     }
                 }
                 var builder = manager.commandBuilder(split[0]);
-                if (split.length == 2) {
-                    builder = builder.literal(split[1]);
+                for (int i = 1; i < split.length; i++) {
+                    builder = builder.literal(split[i]);
                 }
                 if (getPermission() != null) {
                     builder = builder.permission(getPermission());
