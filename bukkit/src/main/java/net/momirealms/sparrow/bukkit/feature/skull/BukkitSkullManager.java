71e657ff235922a8c8ff47a7fa844db511c1b695
g2213swo <2057581537@qq.com>
2024-05-05 17:11:44 +0800

    [Feature] Add skull cache expiration

--- bukkit/src/main/java/net/momirealms/sparrow/bukkit/feature/skull/BukkitSkullManager.java
+++ bukkit/src/main/java/net/momirealms/sparrow/bukkit/feature/skull/BukkitSkullManager.java
@@ -3,16 +3,25 @@ package net.momirealms.sparrow.bukkit.feature.skull;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import net.momirealms.sparrow.bukkit.SparrowBukkitPlugin;
 import net.momirealms.sparrow.bukkit.util.PluginUtils;
 import net.momirealms.sparrow.common.feature.skull.Skull;
 import net.momirealms.sparrow.common.feature.skull.SkullManager;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.TimeUnit;
 
 public final class BukkitSkullManager implements SkullManager {
     private final LoadingCache<String, Skull> skullCache = CacheBuilder.newBuilder()
+            .expireAfterAccess(30, TimeUnit.MINUTES)
             .build(CacheLoader.from(this::getSkullInternal));
 
     private BukkitSkullManager() {
+        Bukkit.getPluginManager().registerEvents(new SkullListener(), SparrowBukkitPlugin.getInstance().getLoader());
     }
 
     private static class SingletonHolder {
@@ -38,5 +47,12 @@ public final class BukkitSkullManager implements SkullManager {
         if (PluginUtils.isPluginEnabled("SkinRestorer"))
             return new SkinRestorerSkull(name);
         return new UserSkull(name);
+    }
+
+    private static class SkullListener implements Listener {
+        @EventHandler
+        private void onPlayerJoin(@NotNull PlayerJoinEvent event) {
+            BukkitSkullManager.getInstance().removeSkull(event.getPlayer().getName());
+        }
     }
 }
