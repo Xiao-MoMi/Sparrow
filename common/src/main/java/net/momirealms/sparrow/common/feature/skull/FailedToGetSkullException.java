b2bb810570e6000d0fa091b1d1717dfb5761a01c
g2213swo <2057581537@qq.com>
2024-05-05 19:44:09 +0800

    [Clean] Clean up code & Add some javadocs

--- common/src/main/java/net/momirealms/sparrow/common/feature/skull/FailedToGetSkullException.java
+++ common/src/main/java/net/momirealms/sparrow/common/feature/skull/FailedToGetSkullException.java
@@ -1,5 +1,8 @@
 package net.momirealms.sparrow.common.feature.skull;
 
+/**
+ * Exception thrown when the skull of a player could not be retrieved.
+ */
 public class FailedToGetSkullException extends RuntimeException {
     public FailedToGetSkullException(String playerName) {
         super("Failed to get skull of the given player name " + playerName + "!");
