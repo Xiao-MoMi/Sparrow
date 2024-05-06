ec9539cb0cf6f4d4f94f6b97b464c12b6f101832
XiaoMoMi <972454774@qq.com>
2024-05-07 07:20:28 +0800

    [Refactor] Versioning

--- common/src/main/java/net/momirealms/sparrow/common/config/ConfigManagerImpl.java
+++ common/src/main/java/net/momirealms/sparrow/common/config/ConfigManagerImpl.java
@@ -2,10 +2,12 @@ package net.momirealms.sparrow.common.config;
 
 import dev.dejvokep.boostedyaml.YamlDocument;
 import dev.dejvokep.boostedyaml.dvs.versioning.BasicVersioning;
+import dev.dejvokep.boostedyaml.dvs.versioning.ManualVersioning;
 import dev.dejvokep.boostedyaml.settings.dumper.DumperSettings;
 import dev.dejvokep.boostedyaml.settings.general.GeneralSettings;
 import dev.dejvokep.boostedyaml.settings.loader.LoaderSettings;
 import dev.dejvokep.boostedyaml.settings.updater.UpdaterSettings;
+import net.momirealms.sparrow.common.config.version.SparrowVersioning;
 import net.momirealms.sparrow.common.plugin.SparrowPlugin;
 
 import java.io.IOException;
@@ -72,7 +74,7 @@ public class ConfigManagerImpl implements ConfigManager {
                     DumperSettings.DEFAULT,
                     UpdaterSettings
                             .builder()
-                            .setVersioning(new BasicVersioning("config-version"))
+                            .setVersioning(new SparrowVersioning("config-version"))
                             .build()
             );
         } catch (IOException e) {
---
+++ common/src/main/java/net/momirealms/sparrow/common/config/version/SparrowVersioning.java
@@ -0,0 +1,21 @@
+package net.momirealms.sparrow.common.config.version;
+
+import dev.dejvokep.boostedyaml.dvs.Pattern;
+import dev.dejvokep.boostedyaml.dvs.segment.Segment;
+import dev.dejvokep.boostedyaml.dvs.versioning.AutomaticVersioning;
+import org.jetbrains.annotations.NotNull;
+
+public class SparrowVersioning extends AutomaticVersioning {
+
+    public static final Pattern PATTERN = new Pattern(
+            Segment.range(0, Integer.MAX_VALUE),
+            Segment.literal("."),
+            Segment.range(0, Integer.MAX_VALUE),
+            Segment.literal("."),
+            Segment.range(0, Integer.MAX_VALUE)
+    );
+
+    public SparrowVersioning(@NotNull String route) {
+        super(PATTERN, route);
+    }
+}
